7811ec0c18858aea7349ead0b9bacc8a5c94c0bd
Van Tai <88.vantai.nguyen@gmail.com>
2023-07-08 18:55:02 +1000

    test1

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ from simple_linear_regr_utils import generate_data
 # Define the input data
 # input_data = {'X_train': X_train.tolist(), 'X_test': X_test.tolist(), 
 # 'y_train': y_train.tolist(), 'y_test': y_test.tolist(), 'hyperparameters': [10000, 0.09]}
-# input_data = [[0.077], [0.068], [0.092]]
-input_data = 0.078
+input_data = [[0.077], [0.068], [0.092]]
+# input_data = 0.078
 # Send an HTTP POST request to the API endpoint
+# response = requests.post('http://3.106.53.174:8080/log')
 response = requests.post('http://3.106.53.174:8080/stream', json={'data': input_data})
-# response = requests.post('http://3.106.53.174:8080/stream', json={'data': input_data})
 
 # response = requests.get('http://localhost:8080/log')
 
