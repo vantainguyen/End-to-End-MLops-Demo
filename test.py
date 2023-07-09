20c8ea254f9a743578b4e1c9cc3e0282e8b2fea3
Van Tai <88.vantai.nguyen@gmail.com>
2023-07-09 15:02:40 +1000

    updated test

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
 import requests
-from simple_linear_regr_utils import generate_data
 
-
-# X_train, y_train, X_test, y_test = generate_data()
 
 # Define the input data
-# input_data = {'X_train': X_train.tolist(), 'X_test': X_test.tolist(), 
-# 'y_train': y_train.tolist(), 'y_test': y_test.tolist(), 'hyperparameters': [10000, 0.09]}
-input_data = [[0.077], [0.068], [0.092]]
-# input_data = 0.078
-# Send an HTTP POST request to the API endpoint
-response = requests.get('http://3.106.53.174:8080/log')
-# response = requests.post('http://3.106.53.174:8080/batch', json={'data': input_data})
 
-# response = requests.get('http://localhost:8080/log')
+input_data_batch = [[0.077], [0.068], [0.092]]
+input_data_stream = 0.078
+# Send an HTTP POST request to the API endpoint
+# response = requests.get('http://3.106.53.174:8080/log')
+# response = requests.post('http://3.106.53.174:8080/batch', json={'data': input_data_batch})
+response = requests.post('http://3.106.53.174:8080/stream', json={'data': input_data_stream})
+
 
 # Retrieve the inference result from the response
 inference_result = response.json()['result']
