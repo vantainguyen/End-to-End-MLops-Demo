26d554e37f9c03a8f108618d2cf67e23c8b95c2c
Van Tai <88.vantai.nguyen@gmail.com>
2023-07-11 18:22:45 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 
-demand = 'log' # 'batch', 'stream', 'log'
+demand = 'stream' # 'batch', 'stream', 'log'
 # Request invocation
 if demand == 'batch':
     input_data_batch = [[0.077], [0.068], [0.092]]
     response = requests.post('http://3.106.53.174:8080/batch', json={'data': input_data_batch})
 elif demand == 'stream':
-    input_data_stream = 0.078
+    input_data_stream = 0.077
     response = requests.post('http://3.106.53.174:8080/stream', json={'data': input_data_stream})
 elif demand == 'log':
     response = requests.get('http://3.106.53.174:8080/log')
