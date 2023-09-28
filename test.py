006964ca0309edad62c6ce018d81fd073035ae31
Van Tai Nguyen <60877890+vantainguyen@users.noreply.github.com>
2023-09-28 14:45:21 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import requests
 
-demand = 'batch' # 'batch', 'stream', 'log', 'emptyLog
-# Request invocation
-if demand == 'batch':
-    input_data_batch = [[0.077], [0.068], [0.092]]
-    response = requests.post('http://13.239.25.119:8080/batch', json={'data': input_data_batch})
-elif demand == 'stream':
-    input_data_stream = 0.077
-    response = requests.post('http://13.239.25.119:8080/stream', json={'data': input_data_stream})
-elif demand == 'log':
-    response = requests.get('http://13.239.25.119:8080/log')
-elif demand == 'emptyLog':
-    response = requests.get('http://13.239.25.119:8080/emptyLog')
+def test_integration():
+    assert 0 == 0
 
-# Retrieve the inference result from the response
-inference_result = response.json()['result']
-print(inference_result)
+# demand = 'batch' # 'batch', 'stream', 'log', 'emptyLog
+# # Request invocation
+# if demand == 'batch':
+#     input_data_batch = [[0.077], [0.068], [0.092]]
+#     response = requests.post('http://13.239.25.119:8080/batch', json={'data': input_data_batch})
+# elif demand == 'stream':
+#     input_data_stream = 0.077
+#     response = requests.post('http://13.239.25.119:8080/stream', json={'data': input_data_stream})
+# elif demand == 'log':
+#     response = requests.get('http://13.239.25.119:8080/log')
+# elif demand == 'emptyLog':
+#     response = requests.get('http://13.239.25.119:8080/emptyLog')
+
+# # Retrieve the inference result from the response
+# inference_result = response.json()['result']
+# print(inference_result)
